481968662b9ebf3aeb1a80fdf82d39a6a2e9a835
Benjamin Sims <arycama@gmail.com>
2023-01-29 20:09:09 +1030

    A reasonably good formulation

--- Assets/MovementTest.cs
+++ Assets/MovementTest.cs
@@ -1,38 +1,65 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
+using static UnityEngine.Mathf;
+using static UnityEngine.Vector3;
 
 public class MovementTest : MonoBehaviour
 {
     [SerializeField] private float acceleration = 1f;
-    [SerializeField] private Camera camera;
+    [SerializeField] private bool test = false;
     [SerializeField] private Rigidbody rigidbody;
+    [SerializeField] private Camera camera;
 
-    private float target;
+    private Vector3 targetPosition;
+
+    private void OnEnable()
+    {
+        targetPosition = transform.position;
+    }
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        //if (!Input.GetMouseButtonDown(0))
+       //     return;
+
+        var ray = camera.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out var hit))
         {
-            if (Physics.Raycast(camera.ScreenPointToRay(Input.mousePosition), out var hit))
-            {
-                target = hit.point.x;
-            }
+            //targetPosition = new Vector3(hit.point.x, rigidbody.position.y, rigidbody.position.z);
+            targetPosition = hit.point;
         }
     }
 
+    public float force;
+
     private void FixedUpdate()
     {
-        // Need to find correct value for this
-        var t = Time.fixedDeltaTime;
-        var position = rigidbody.position.x;
+        var targetDirection = Normalize(targetPosition - rigidbody.position);
+        var distance = Distance(targetPosition, rigidbody.position);
 
-        var distance = Mathf.Abs(target - position);
-        var targetVelocity = Mathf.Sqrt(2f * acceleration * distance) * Mathf.Sign(target - position);
-        var velocityDelta = targetVelocity - rigidbody.velocity.x;
-        var force = Mathf.Clamp(velocityDelta / t, -acceleration, acceleration);
+        var a = acceleration;
+        var dt = Time.fixedDeltaTime;
+        var x = distance;
 
-        rigidbody.AddForce(force, 0f, 0f, ForceMode.Acceleration);
+        // This accounts for the error introduced by dt
+        var currentSpeed = 0.5f * a * (Sqrt((a * dt * dt + 8f * x) / a) - dt);
+
+        // Time to reach target at current distance+ideal velocity
+        var currentTime = (Sqrt(a * a * dt * dt + 8 * a * x) - a * dt) / (2 * a);
+
+        // One frame forward
+        var nextTime = currentTime - dt;
+
+        // This is the distance goal for the next frame/timestep
+        var nextDistance = 0.5f * a * nextTime * (nextTime + dt);
+
+        // Now calcualte the speed required to get us to the target distance
+        var targetDisplacement = Abs(nextDistance - distance);
+        var targetSpeed = 2 * targetDisplacement / dt - currentSpeed;
+
+
+        var velocityDelta = (targetSpeed * targetDirection - rigidbody.velocity) / dt;
+        var force = ClampMagnitude(velocityDelta, a);
+        this.force = force.magnitude;
+        rigidbody.AddForce(force, ForceMode.Acceleration);
     }
 }
