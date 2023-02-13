601e9a73d209a3193c73ac5fee9bbe02fbdb7cec
Ben Sims <arycama@gmail.com>
2023-02-14 09:45:02 +1030

    Fixed a compile error

--- Assets/MovementTest.cs
+++ Assets/MovementTest.cs
@@ -29,7 +29,7 @@ public class MovementTest : MonoBehaviour
         var targetSpeed = Mathf.Sqrt(2f * acceleration * distance);
 
         // Assuming constant decel, calculate where we want to be next frame, instead of current
-        targetSpeed = Mathf.Max(0f, targetSpeed - acceleration * Time.fixedDeltaTime));
+        targetSpeed = Mathf.Max(0f, targetSpeed - acceleration * Time.fixedDeltaTime);
 
         var direction = Vector3.Normalize(targetPosition - rigidbody.position);
 
