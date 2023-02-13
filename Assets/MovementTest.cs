f9bc1d75205b65527703263b4632342aaa9b6460
Ben Sims <arycama@gmail.com>
2023-02-14 09:44:34 +1030

    Think I'm done with this now

--- Assets/MovementTest.cs
+++ Assets/MovementTest.cs
@@ -1,6 +1,4 @@
 using UnityEngine;
-using static UnityEngine.Mathf;
-using static UnityEngine.Vector3;
 
 public class MovementTest : MonoBehaviour
 {
@@ -13,8 +11,8 @@ public class MovementTest : MonoBehaviour
 
     private void Update()
     {
-        if (!Input.GetMouseButtonDown(0))
-            return;
+        //if (!Input.GetMouseButtonDown(0))
+        //    return;
 
         var ray = camera.ScreenPointToRay(Input.mousePosition);
         if (Physics.Raycast(ray, out var hit))
@@ -23,46 +21,25 @@ public class MovementTest : MonoBehaviour
         }
     }
 
-    public float p, p0, h, t, t0, t1, x, d0, d1, mid, y, targetDisplacement, dist, force;
-
     private void FixedUpdate()
     {
-        var a = acceleration;
-        var v = rigidbody.velocity.magnitude;
-        p = Distance(targetPosition, rigidbody.position);
-        var d = Time.fixedDeltaTime;
+        var distance = Vector3.Distance(rigidbody.position, targetPosition);
 
-        p0 = v * (d * a + v) / (2 * a) + p;
-        h = p0 / 2;
+        // Calculate the max velocity we can have and still reach the target with constant decel, based on distance
+        var targetSpeed = Mathf.Sqrt(2f * acceleration * distance);
 
-        t = Sqrt(p0 / a);
+        // Assuming constant decel, calculate where we want to be next frame, instead of current
+        targetSpeed = Mathf.Max(0f, targetSpeed - acceleration * Time.fixedDeltaTime));
 
-        t0 = Sqrt(Sq(a) * Sq(d) + 8 * a * p) / (2 * a) - d / 2;
-        t1 = 2 * t - Sqrt(Max(0, 8 * Sq(a) * Sq(t) - Sq(a) * Sq(d) - 8 * a * p)) / (2 * a) - d / 2;
+        var direction = Vector3.Normalize(targetPosition - rigidbody.position);
 
-        x = p <= h ? t0 : t1;
-        x -= Time.fixedDeltaTime;
+        // Calculate acceleration required to get us to the desired velocity in a single frame
+        var targetVelocity = direction * targetSpeed;
+        var force = (targetVelocity - rigidbody.velocity) / Time.fixedDeltaTime;
 
-        d0 = a * x * (x + d) / 2;
-        d1 = -a * (2 * Sq(x) + 2 * d * x - 8 * x * t + Sq(d) + 4 * Sq(t) - 4 * d * t) / 4;
-        mid = Sqrt(Sq(a) * Sq(d) + 8 * a * h) / (2 * a) - d / 2;
-        y = p < mid ? d0 : d1;
+        // Clamp to max accel
+        var clampedForce = Vector3.ClampMagnitude(force, acceleration);
 
-        targetDisplacement = Abs(p - y);
-
-        var targetDirection = Normalize(targetPosition - rigidbody.position);
-        // force = (targetDisplacement - v * Time.fixedDeltaTime) / Sq(Time.fixedDeltaTime);
-
-        //force = (targetDisplacement - 2f * v * d) / Sq(d);
-        //force = (targetDisplacement - v - d * v) / (0.5f * (1 + d)) / d;
-
-        dist = p0 - y;
-
-        force = Abs(p0 - y) / (0.5f * x * (x * d));
-
-        var clampedForce = ClampMagnitude(targetDirection * force, acceleration);
         rigidbody.AddForce(clampedForce, ForceMode.Acceleration);
     }
-
-    public static float Sq(float x) => x * x;
 }
