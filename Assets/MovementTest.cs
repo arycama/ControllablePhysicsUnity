81c579c4565c33c57dc1e836399e4d474b67daf6
Ben Sims <arycama@gmail.com>
2023-02-15 23:40:13 +1030

    Added in the correct delta time equation

--- Assets/MovementTest.cs
+++ Assets/MovementTest.cs
@@ -27,6 +27,7 @@ public class MovementTest : MonoBehaviour
 
         // Calculate the max velocity we can have and still reach the target with constant decel, based on distance
         var targetSpeed = Mathf.Sqrt(2f * acceleration * distance);
+        targetSpeed = 0.5f * acceleration * (Mathf.Sqrt((acceleration * Time.fixedDeltaTime * Time.fixedDeltaTime + 8f * distance) / acceleration) - Time.fixedDeltaTime);
 
         // Assuming constant decel, calculate where we want to be next frame, instead of current
         targetSpeed = Mathf.Max(0f, targetSpeed - acceleration * Time.fixedDeltaTime);
